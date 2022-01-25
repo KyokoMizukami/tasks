fe7e548a55f52d0f65830fe84fea4b3405ea47ab
KyokoMizukami <onthewater315@gmail.com>
2022-01-24 16:42:41 -0800

    01.24

--- 1.24/script.js
+++ 1.24/script.js
@@ -1,14 +1,14 @@
 /* JavaScript DOM Exercises 01 */
-
+const paragraph = document.querySelector("p");
+const word = paragraph.innerText.split(" ");
 /*
   Exercise 01
   -----------
   Highlight all of the words over 8 characters long in the paragraph text (with a yellow background for example)
 */
 
-const paragraph = document.querySelector("p");
+
 paragraph.innerHTML = paragraph.innerText;
-const word = paragraph.innerText.split(" ");
 console.log(word);
 
 for (let i = 0; i < word.length; i++) {
