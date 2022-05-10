9b60c51990d9568e7fb2be5bbd1483504ba9354f
KyokoMizukami <onthewater315@gmail.com>
2022-05-10 22:17:30 +0900

    05.09

---
+++ 5.10/employee.js
@@ -0,0 +1,10 @@
+const employee = (name, email, department, startDate) => {
+  const employeeInfo = {
+    name, email, department, startDate
+  }
+  return employeeInfo
+};
+
+
+module.exports = { employee };
+
